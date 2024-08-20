29d156ff7ad9fa734d43b7f05e15d57c587eb3c7
Toficzak <toficzak@duck.com>
2024-08-20 18:48:08 +0200

    Add simple minio connector and storage tests

--- api/src/test/java/org/mandulis/mts/controller/AttachmentControllerMinioIT.java
+++ api/src/test/java/org/mandulis/mts/controller/AttachmentControllerMinioIT.java
@@ -1,40 +1,33 @@
 package org.mandulis.mts.controller;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.AfterEach;
+import io.minio.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mandulis.mts.entity.Attachment;
 import org.mandulis.mts.exception.AttachmentWithoutNameException;
 import org.mandulis.mts.exception.EmptyAttachmentException;
+import org.mandulis.mts.exception.StorageException;
 import org.mandulis.mts.repository.AttachmentRepository;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.containers.MinIOContainer;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class AttachmentControllerIT {
-
-    @Value("${storage.location}")
-    private String storageLocation;
+class AttachmentControllerMinioIT extends BaseMinioIT{
 
     @Autowired
     private MockMvc mockMvc;
@@ -54,17 +47,33 @@ class AttachmentControllerIT {
         databaseHelper.clearCategories();
         databaseHelper.clearTickets();
         databaseHelper.clearUsers();
+
+        removeTestBucket();
     }
 
-    @AfterEach
-    void after() throws IOException {
-        File testFileStorage = new File(storageLocation);
-        FileUtils.deleteDirectory(testFileStorage);
+    private void removeTestBucket() {
+
+        boolean exists;
+        try {
+            exists = MINIO_CLIENT.bucketExists(BucketExistsArgs.builder().bucket(BUCKET_NAME).build());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (!exists) {
+            return;
+        }
+
+        try {
+            MINIO_CLIENT.removeBucket(RemoveBucketArgs.builder().bucket(BUCKET_NAME).build());
+        } catch (Exception e) {
+            throw new RuntimeException("Cannot remove objects from bucket.");
+        }
     }
 
     @Test
         // todo: maybe there is some property to keep sexy names?
-    void shouldUploadAttachment() throws Exception {
+    void shouldThrowException_bucketDoesNotExist() throws Exception {
         // given
         var category = databaseHelper.setupCategory();
         var user = databaseHelper.setupUser();
@@ -75,22 +84,43 @@ class AttachmentControllerIT {
         MockMultipartFile file = new MockMultipartFile("file", filename, "text/plain", fileContent);
 
         String url = String.format("/api/v1/user/tickets/%d/attachments", ticket.getId());
-        // when
+        // when, then
+        mockMvc.perform(multipart(url).file(file))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.content[0].error").value(
+                        // todo: weird formatting, check out later
+                        String.format("%s: Bucket[name=%s] does not exist.", StorageException.class.getCanonicalName(), BUCKET_NAME)));
+    }
+
+    @Test
+        // todo: maybe there is some property to keep sexy names?
+    void shouldUploadAttachment() throws Exception {
+        // given
+        MINIO_CLIENT.makeBucket(MakeBucketArgs.builder().bucket(BUCKET_NAME).build());
+
+        var category = databaseHelper.setupCategory();
+        var user = databaseHelper.setupUser();
+        var ticket = databaseHelper.setupTicket(user, category);
+
+        String filename = "filename.txt";
+        byte[] fileContent = "some xml".getBytes();
+        MockMultipartFile file = new MockMultipartFile("file", filename, "text/plain", fileContent);
+
+        String url = String.format("/api/v1/user/tickets/%d/attachments", ticket.getId());
+        // when, then
         mockMvc.perform(multipart(url).file(file))
                 .andExpect(status().isCreated());
 
-        // then
         var attachments = attachmentRepository.findAll();
         assertEquals(1, attachments.size());
         Attachment dbAttachment = attachments.getFirst();
         assertEquals(filename, dbAttachment.getFileName());
-        String storedFilePath = storageLocation + "/" + filename;
+        String storedFilePath = BUCKET_NAME + "/" + filename;
         assertEquals(storedFilePath, dbAttachment.getFilePath());
         assertEquals(ticket.getId(), dbAttachment.getTicket().getId());
 
-        File storedFile = new File(storedFilePath);
-        assertTrue(storedFile.exists() && !storedFile.isDirectory());
-        assertArrayEquals(fileContent, Files.readAllBytes(storedFile.toPath()));
+        GetObjectResponse response = MINIO_CLIENT.getObject(GetObjectArgs.builder().bucket(BUCKET_NAME).object(filename).build());
+        assertArrayEquals(fileContent, response.readAllBytes());
     }
 
     @Test
