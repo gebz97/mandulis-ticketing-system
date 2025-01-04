21be19609f6c4f6fd7f69a7e1102b24b267faea5
stohirov <suxrobtoxirov001@gmail.com>
2025-01-04 07:21:46 +0500

    fix: removed unneccessary import in Service

--- api/src/main/java/org/mandulis/mts/category/CategoryService.java
+++ api/src/main/java/org/mandulis/mts/category/CategoryService.java
@@ -3,7 +3,6 @@ package org.mandulis.mts.category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
